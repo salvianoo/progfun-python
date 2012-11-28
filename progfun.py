8424bb5c05f5b9ed0e4e2a954fd42c4512bb8f84
Salviano Ludgério <salviano.lfg@gmail.com>
2012-11-28 18:16:32 -0200

    Add new methods and tests

--- progfun.py
+++ progfun.py
@@ -20,11 +20,25 @@ def fib(n):
         return fib(n - 1) + fib(n - 2)
 
 
-def vetor_maioria(arr, index):
-    arr_maioria = filter(lambda i: i == arr[index], arr)
-    return len(arr_maioria) > len(arr) / 2
+# def vetor_maioria(arr, index):
+#     arr_maioria = filter(lambda i: i == arr[index], arr)
+#     return len(arr_maioria) > len(arr) / 2
 
 
 def div_by_three_or_five(arr):
     cond = lambda el: el % 3 == 0 or el % 5 == 0
     return reduce(lambda res, el: res + el, filter(cond, arr))
+
+
+def primo(num, arr):
+    return len(filter(lambda el: num % el == 0, arr)) == 2
+
+
+def elemento_maioria(arr, index):
+    return len(filter(lambda el: el == arr[index], arr)) > len(arr) / 2
+
+
+def vetor_maioria(arr):
+    return True in map(lambda elemento:
+            len(filter(lambda el_maioria:
+                el_maioria == elemento, arr)) > len(arr) / 2, arr)
--- test_case.py
+++ test_case.py
@@ -1,5 +1,5 @@
 import unittest
-from progfun import factorial, fib, vetor_maioria, div_by_three_or_five
+from progfun import factorial, fib, vetor_maioria, div_by_three_or_five, primo
 
 
 class TestAlgoritmos(unittest.TestCase):
@@ -13,17 +13,13 @@ class TestAlgoritmos(unittest.TestCase):
     def test_factorial_de_oito(self):
         self.assertEqual(40320, factorial(8))
 
-    def test_item_1_eh_maioria(self):
+    def test_vetor_eh_maioria(self):
         arr = [1, 1, 1, 1, 1, 3, 4, 5, 6]
-        self.assertEqual(True, vetor_maioria(arr, arr.index(1)))
+        self.assertEqual(True, vetor_maioria(arr))
 
-    def test_item_1_nao_eh_maioria(self):
-        arr = [1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
-        self.assertEqual(False, vetor_maioria(arr, arr.index(1)))
-
-    def test_item_2_eh_maioria(self):
-        arr = [1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
-        self.assertEqual(True, vetor_maioria(arr, arr.index(2)))
+    def test_vetor_nao_eh_maioria(self):
+        arr = range(10)
+        self.assertEqual(False, vetor_maioria(arr))
 
     def test_fibonacci_de_zero(self):
         self.assertEqual(0, fib(0))
@@ -39,6 +35,12 @@ class TestAlgoritmos(unittest.TestCase):
 
     def test_div_by_three_or_five_with_array_of_1000(self):
         self.assertEqual(233168, div_by_three_or_five(range(1000)))
+
+    def test_numero_3_eh_primo(self):
+        self.assertEqual(True, primo(3, range(1, 100)))
+
+    def test_numero_4_nao_eh_primo(self):
+        self.assertEqual(False, primo(4, range(1, 100)))
 
 if __name__ == "__main__":
     unittest.main()
